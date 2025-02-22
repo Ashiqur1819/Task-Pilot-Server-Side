c147b1b21d7cde95261ca07e46ab758b87559ceb
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-02-22 16:41:09 +0600

    Done full website

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("TaskPilotDB").collection("users");
     const taskCollection = client.db("TaskPilotDB").collection("tasks");
 
-    app.get("/users", async(req, res) => {
-        const cursor = userCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post("/users", async(req, res) => {
+    app.post("/users", async (req, res) => {
       const newUser = req.body;
       const query = { email: newUser?.email };
       const isAlreadyExist = await userCollection.findOne(query);
@@ -44,56 +44,55 @@ async function run() {
         const result = await userCollection.insertOne(newUser);
         res.send(result);
       }
-    })
+    });
 
-     app.get("/tasks", async (req, res) => {
-       const cursor = taskCollection.find();
-       const result = await cursor.toArray();
-       res.send(result);
-     });
+    app.get("/tasks", async (req, res) => {
+      const cursor = taskCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     app.post("/task", async(req, res) => {
+    app.post("/tasks", async (req, res) => {
       const task = req.body;
-      const result = await taskCollection.insertOne(task)
-      res.send(result)
-     })
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
 
-     app.delete("/tasks/:id", async (req, res) => {
-       const id = req.params.id;
-       const query = { _id: new ObjectId(id) };
-       const result = await taskCollection.deleteOne(query);
-       res.send(result);
-     });
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
-     app.get("/tasks/:id", async (req, res) => {
-       const id = req.params.id;
-       const query = { _id: new ObjectId(id) };
-       const result = await taskCollection.find(query).toArray();
-       res.send(result);
-     });
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
 
-     app.put("/tasks/:id", async (req, res) => {
-       const id = req.params.id;
-       const filter = { _id: new ObjectId(id) };
-       const task = req.body;
-       const updatedTask = {
-         $set: {
-           title: task.title,
-           description: task.description,
-           timestamp: task.timestamp,
-           category: task.category,
-         },
-       };
-       const result = await taskCollection.updateOne(filter, updatedTask);
-       res.send(result);
-     });
-
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const task = req.body;
+      const updatedTask = {
+        $set: {
+          title: task.title,
+          description: task.description,
+          timestamp: task.timestamp,
+          category: task.category,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedTask);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
